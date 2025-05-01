097459c4847a6b0ae3f78fb0f3d7870cea394d97
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-05-01 21:05:19 +0700

    feat: add emailService

---
+++ service/emailService.js
@@ -0,0 +1,25 @@
+const kafka = require("../config/kafkaClient");
+
+const producer = kafka.producer();
+
+const sendVerificationEmail = async (user) => {
+  try {
+    await producer.connect();
+    await producer.send({
+      topic: "verification_emails",
+      messages: [
+        {
+          key: user.email,
+          value: JSON.stringify(user),
+        },
+      ],
+    });
+    console.log(
+      `[Kafka Producer] Sent verification email event for ${user.email}`
+    );
+  } catch (err) {
+    console.error("[Kafka Producer] Failed to send message:", err);
+  }
+};
+
+module.exports = { sendVerificationEmail };
